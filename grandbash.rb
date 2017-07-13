301d8a564415d9c09d5fa32ed3b94332dd114244
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-13 16:54:49 -0400

    working grandbash file

--- grandbash.rb
+++ grandbash.rb
@@ -1,16 +1,32 @@
+# def winning_number(my_ticket, winning_nums)
+	
+# # p my_ticket
+# # p winning_nums
+
+
+# yacht = []
+# if winning_nums.include?(my_ticket)
+# yacht << my_ticket
+# end
+
+# yacht
+
+# end
+
 def winning_number(my_ticket, winning_nums)
 	
-p my_ticket
-p winning_nums
+# p my_ticket
+# p winning_nums
 
 
 yacht = []
-if winning_nums.include?(my_ticket)
-yacht << my_ticket
+
+winning_nums.each do |item|
+	if item == my_ticket
+		yacht << my_ticket
+	end
+
 end
 
 yacht
-
 end
-
-
