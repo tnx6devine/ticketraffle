ad9b9fb369bc3a12091ea27bbd95c38492566a9d
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-14 11:58:40 -0400

    ticket with so close added

--- grandbash.rb
+++ grandbash.rb
@@ -23,10 +23,37 @@ yacht = []
 
 winning_nums.each do |item|
 	if item == my_ticket
-		yacht << my_ticket
+		yacht << item
 	end
 
 end
 
 yacht
+end
+
+
+def winner_array(my_ticket, winning_nums)
+[]
+end
+
+def so_close(my_ticket, winning_nums)
+	counter = 0
+	
+		
+		if winning_nums[0] == my_ticket[0]
+			counter = counter + 1
+end
+		if winning_nums[1] == my_ticket[1]
+			counter = counter + 1
+		end
+		if winning_nums[2] == my_ticket[2]
+			counter = counter + 1
+		end
+		if winning_nums[3] == my_ticket[3]
+			counter = counter + 1
+		end
+	
+	
+	counter == 3
+
 end
--- grandbash_test.rb
+++ grandbash_test.rb
@@ -10,14 +10,32 @@ class TestGrandBash < Minitest::Test
 		my_ticket="1234"
 		winning_nums=["2222", "3333", "4444"]
 		assert_equal([], winning_number(my_ticket, winning_nums))
-		p winning_number(my_ticket, winning_nums)
+		
 	end
 
 
 	def test_assert_that_my_ticket_wins
 		my_ticket="1111"
-		winning_nums=["1111", "2222", "3333", "4444"]
+		winning_nums =["1111", "2222", "3333", "4444"]
 		assert_equal(["1111"], winning_number(my_ticket, winning_nums))
 	end
 
+	def test_assert_that_my_ticket_is_so_close_false
+		my_ticket = "1234"
+		winning_nums = "1222"
+		assert_equal(false, so_close(my_ticket, winning_nums))
+	end
+
+	def test_assert_that_my_ticket_is_so_close_true
+		my_ticket = "1234"
+		winning_nums ="1239"
+		assert_equal(true, so_close(my_ticket, winning_nums))
+	end
+
+	def test_assert_that_winner_array_is_array
+		my_ticket = "1234"
+		winning_nums ="1239"
+		assert_equal(Array, winner_array(my_ticket, winning_nums).class)
+	end
 end
+
